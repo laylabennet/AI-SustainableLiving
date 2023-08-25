02850c4cfca9888f2bf6f803dd27c4aae8a97f30
Layla Bennet <142955490+laylabennet@users.noreply.github.com>
2023-08-24 23:42:16 -0500

    Create data_preprocessing.py

---
+++ utils/data_preprocessing.py
@@ -0,0 +1,9 @@
+class DataPreprocessing:
+    @staticmethod
+    def preprocess_environmental_data(data):
+        # Placeholder logic for preprocessing environmental data
+        preprocessed_data = {
+            "temperature": data["temperature"],
+            "air_quality": data["air_quality"]
+        }
+        return preprocessed_data
